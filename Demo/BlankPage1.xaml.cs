4694c8c813bd7cc9e0c41b3c685129809b22491e
chenrensong <chenrensong@outlook.com>
2015-10-16 02:02:31 +0800

    修改demo

--- Demo/BlankPage1.xaml.cs
+++ Demo/BlankPage1.xaml.cs
@@ -5,8 +5,6 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
-using Windows.Storage;
-using Windows.System;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -15,26 +13,24 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
+// “空白页”项模板在 http://go.microsoft.com/fwlink/?LinkId=234238 上提供
+
 namespace Demo
 {
-
-    public sealed partial class MainPage : Page
+    /// <summary>
+    /// 可用于自身或导航至 Frame 内部的空白页。
+    /// </summary>
+    public sealed partial class BlankPage1 : Page
     {
-
-        public MainPage()
+        public BlankPage1()
         {
             this.InitializeComponent();
-            base.Loaded += MainPage_Loaded;
-            this.NavigationCacheMode = NavigationCacheMode.Disabled;
         }
 
-        private void MainPage_Loaded(object sender, RoutedEventArgs e)
+        private void button_Click(object sender, RoutedEventArgs e)
         {
-        }
+            this.Frame.Navigate(typeof(MainPage));
 
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            this.Frame.GoBack();
         }
     }
 }
